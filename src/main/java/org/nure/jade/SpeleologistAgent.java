3b16da3fa850b5bcd4f88c285a3f8e76e8e76f71
Alexander Greckov <alexander.greckov@raccoongang.com>
2024-01-04 08:24:10 +0200

    Code refactoring

--- src/main/java/org/nure/jade/SpeleologistAgent.java
+++ src/main/java/org/nure/jade/SpeleologistAgent.java
@@ -27,15 +27,14 @@ public class SpeleologistAgent extends Agent {
 
 	@Override
 	protected void takeDown() {
-		System.out.println("Speleologist-agent " + getAID().getName() + "is terminating.");
-		try {
+		System.out.println(agentMessagePrefix + getAID().getName() + "is terminating.");
+
+        try {
 			getContainerController().getAgent(navigatorAid.getLocalName()).kill();
 			getContainerController().getAgent(environmentAid.getLocalName()).kill();
 		} catch(ControllerException e) {
-			throw new RuntimeException(e);
+			System.exit(0);
 		}
-
-		System.exit(0);
 	}
 
 	@Override
@@ -93,117 +92,143 @@ public class SpeleologistAgent extends Agent {
 			}
 		}
 
+		private void askEnvState() {
+			discover();
+
+			ACLMessage request = new ACLMessage(ACLMessage.REQUEST);
+			request.setContent("Current state");
+
+			request.addReceiver(environmentAid);
+
+			myAgent.send(request);
+
+			mt = MessageTemplate.and(MessageTemplate.MatchPerformative(ACLMessage.INFORM),
+					MessageTemplate.MatchReplyTo(new AID[]{myAgent.getAID()}));
+
+			System.out.println(agentMessagePrefix + "Request for current state sent.");
+
+			step = 1;
+		}
+
+		private void receiveEnvState() {
+			ACLMessage reply = myAgent.receive(mt);
+
+			if(reply != null) {
+
+				if(reply.getPerformative() == ACLMessage.INFORM) {
+					String state = reply.getContent();
+
+					System.out.println(agentMessagePrefix + "Received state info from environment = " + state);
+
+					try {
+						currentState = objectMapper.readValue(state, State.class);
+					} catch (JsonProcessingException e) {
+						throw new RuntimeException(e);
+					}
+				}
+				step = 2;
+				return;
+			}
+
+			block();
+		}
+
+		private void sendStateToNavigator() {
+			ACLMessage state = new ACLMessage(ACLMessage.INFORM);
+
+			state.setLanguage("English");
+			state.setOntology("WumpusWorld");
+
+			state.addReceiver(navigatorAid);
+			String feelings = speech.tellPercept(currentState.percept);
+			state.setContent(feelings);
+
+			myAgent.send(state);
+
+			mt = MessageTemplate.and(MessageTemplate.MatchPerformative(ACLMessage.PROPOSE),
+					MessageTemplate.MatchReplyTo(new AID[]{myAgent.getAID()}));
+
+			System.out.println(agentMessagePrefix + "Sent state to the navigator agent");
+			step = 3;
+		}
+
+		private void receiveNewNavigatorAction() {
+			ACLMessage reply2 = myAgent.receive(mt);
+			if(reply2 != null) {
+				String action = reply2.getContent();
+				wumpusAction = speech.recognizeAction(action);
+				System.out.println(agentMessagePrefix + "Received new action from navigator = " + action);
+				step = 4;
+				return;
+			}
+
+			block();
+		}
+
+		private void sendActionToEnv() {
+			ACLMessage action = new ACLMessage(ACLMessage.CFP);
+			action.setConversationId("environment");
+			action.addReceiver(environmentAid);
+			action.setContent(wumpusAction.getSymbol());
+			myAgent.send(action);
+
+			mt = MessageTemplate.and(
+					MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL),
+					MessageTemplate.MatchReplyTo(new AID[]{myAgent.getAID()})
+			);
+
+			step = 5;
+		}
+
+		private void receiveOkFromEnv() {
+			ACLMessage envReply = myAgent.receive(mt);
+			if(envReply != null) {
+				if(envReply.getPerformative() == ACLMessage.ACCEPT_PROPOSAL) {
+					if(WumpusAction.CLIMB.equals(wumpusAction)) {
+						System.out.println(agentMessagePrefix + "Climbed out of the cave.");
+						step = 6;
+					} else {
+						System.out.println(agentMessagePrefix + "Going to the zero step.");
+						step = 0;
+					}
+
+				}
+
+				return;
+			}
+
+			block();
+		}
+
+		private void gameIsEnded() {
+			System.out.println(agentMessagePrefix + "Game is ended.");
+			step = GAME_EXIT;
+			myAgent.doDelete();
+		}
+
 		@Override
 		public void action() {
 			switch(step) {
 				case ASK_ENV_STATE:
-					discover();
-
-					ACLMessage request = new ACLMessage(ACLMessage.REQUEST);
-					request.setContent("Current state");
-
-					request.addReceiver(environmentAid);
-
-					myAgent.send(request);
-
-					mt = MessageTemplate.and(MessageTemplate.MatchPerformative(ACLMessage.INFORM),
-							MessageTemplate.MatchReplyTo(new AID[]{myAgent.getAID()}));
-
-					System.out.println(agentMessagePrefix + "Request for current state sent.");
-
-					step = 1;
-
+					askEnvState();
 					break;
 				case RECEIVE_ENV_STATE:
-					ACLMessage reply = myAgent.receive(mt);
-
-					if(reply != null) {
-
-						if(reply.getPerformative() == ACLMessage.INFORM) {
-							String state = reply.getContent();
-
-							System.out.println(agentMessagePrefix + "Received state info from environment = " + state);
-
-                            try {
-                                currentState = objectMapper.readValue(state, State.class);
-                            } catch (JsonProcessingException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-						step = 2;
-						return;
-					}
-
-					block();
+					receiveEnvState();
 					break;
 				case SEND_STATE_TO_NAVIGATOR:
-					ACLMessage state = new ACLMessage(ACLMessage.INFORM);
-
-					state.setLanguage("English");
-					state.setOntology("WumpusWorld");
-
-					state.addReceiver(navigatorAid);
-					String feelings = speech.tellPercept(currentState.percept);
-					state.setContent(feelings);
-
-					myAgent.send(state);
-
-					mt = MessageTemplate.and(MessageTemplate.MatchPerformative(ACLMessage.PROPOSE),
-							MessageTemplate.MatchReplyTo(new AID[]{myAgent.getAID()}));
-
-					System.out.println(agentMessagePrefix + "Sent state to the navigator agent");
-					step = 3;
+					sendStateToNavigator();
 					break;
 				case RECEIVE_NEW_NAVIGATOR_ACTION:
-					ACLMessage reply2 = myAgent.receive(mt);
-					if(reply2 != null) {
-						String action = reply2.getContent();
-						wumpusAction = speech.recognizeAction(action);
-						System.out.println(agentMessagePrefix + "Received new action from navigator = " + action);
-						step = 4;
-						return;
-					}
-
-					block();
+					receiveNewNavigatorAction();
 					break;
 				case SEND_ACTION_TO_ENV:
-					ACLMessage action = new ACLMessage(ACLMessage.CFP);
-					action.setConversationId("environment");
-					action.addReceiver(environmentAid);
-					action.setContent(wumpusAction.getSymbol());
-					myAgent.send(action);
-
-					mt = MessageTemplate.and(
-							MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL),
-							MessageTemplate.MatchReplyTo(new AID[]{myAgent.getAID()})
-					);
-
-					step = 5;
+					sendActionToEnv();
 					break;
 				case RECEIVE_OK_FROM_ENV:
-					ACLMessage envReply = myAgent.receive(mt);
-					if(envReply != null) {
-						if(envReply.getPerformative() == ACLMessage.ACCEPT_PROPOSAL) {
-							if(WumpusAction.CLIMB.equals(wumpusAction)) {
-								System.out.println(agentMessagePrefix + "Climbed out of the cave.");
-								step = 6;
-							} else {
-								System.out.println(agentMessagePrefix + "Going to the zero step.");
-								step = 0;
-							}
-
-						}
-
-						return;
-					}
-
-					block();
+					receiveOkFromEnv();
 					break;
 				case GAME_IS_ENDED:
-					System.out.println(agentMessagePrefix + "Game is ended.");
-					step = GAME_EXIT;
-					myAgent.doDelete();
-					break;
+					gameIsEnded();
 			}
 		}
 
